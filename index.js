4eed6bad45c95192d9c095ac1a83a28df990f55c
jessicasalbert <jessicarsalbert@gmail.com>
2020-10-08 12:56:07 -0400

    completed third deliverable

--- index.js
+++ index.js
@@ -21,8 +21,13 @@ function submitData(name, email) {
     .then(function(object) {
     document.body.innerHTML = `${object.id}`;
     })
+    .catch(function(error) {
+        document.body.innerHTML = `${error}`;
+      });
+    
     
 }
+
 
 
 
